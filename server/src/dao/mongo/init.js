f191c89ed3d0a306c08e986952bb091225999b5d
ThanigaiselvanN <135937256+ThanigaiselvanN@users.noreply.github.com>
2023-07-21 11:48:55 +0530

    Update init.js

--- server/src/dao/mongo/init.js
+++ server/src/dao/mongo/init.js
@@ -4,7 +4,7 @@ import { createMongoUri } from "./../../utils/URIUtil";
 
 mongoose.Promise = global.Promise;
 const mongoConfig = config.get('mongo');
-var uri = "mongodb://lyncspace:ssaug%4029@cluster0-shard-00-00-b3lls.mongodb.net:27017,cluster0-shard-00-01-b3lls.mongodb.net:27017,cluster0-shard-00-02-b3lls.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+var uri = "";
 
 mongoose.connect(uri, function (err) {
     console.log("MongoDB Connection URI: ", createMongoUri(mongoConfig));
